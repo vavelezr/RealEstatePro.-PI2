bccad0b578dbce5f610c197c8dff60dac7800359
Luisa Polanco <luipolanco1229@gmail.com>
2024-09-07 13:50:02 -0500

    tests to user functionality added

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,52 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+User = get_user_model()
+
+
+class UserAuthTests(TestCase):
+    def setUp(self):
+        self.username = 'testuser'
+        self.password = 'testpassword'
+        self.user = User.objects.create_user(username=self.username, password=self.password)
+        self.login_url = reverse('login')
+        self.signup_url = reverse('signup')
+
+    def test_login_view_get(self):
+        response = self.client.get(self.login_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/login.html')
+
+    def test_login_view_post_success(self):
+        response = self.client.post(self.login_url, {'username': self.username, 'password': self.password})
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('home'))
+
+    def test_login_view_post_failure(self):
+        response = self.client.post(self.login_url, {'username': self.username, 'password': 'wrongpassword'})
+        self.assertEqual(response.status_code, 200)
+        self.assertFormError(response, 'form', None, 'Please enter a correct username and password.')
+
+    def test_signup_view_get(self):
+        response = self.client.get(self.signup_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/signup.html')
+
+    def test_signup_view_post_success(self):
+        response = self.client.post(self.signup_url, {
+            'username': 'newuser',
+            'password1': 'newpassword',
+            'password2': 'newpassword'
+        })
+        self.assertEqual(response.status_code, 302) 
+        self.assertTrue(User.objects.filter(username='newuser').exists())
+
+    def test_signup_view_post_failure(self):
+        response = self.client.post(self.signup_url, {
+            'username': 'newuser',
+            'password1': 'newpassword',
+            'password2': 'differentpassword'
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertFormError(response, 'form', 'password2', 'The two password fields must match.')
