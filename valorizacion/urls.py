28ffa974b182341f0726cc772155b7876aae7311
Luis Miguel Giraldo Gonzalez <lgiraldo1109@gmail.com>
2024-09-07 14:42:50 -0500

    tools of good practices (Luism)

--- valorizacion/urls.py
+++ valorizacion/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("calculo/", views.calculo, name="calculo"),
+    path("calculate/", views.calculate, name="calculate"),
 ]
