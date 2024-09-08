f8f33b98e55db38f71c4a0848ad4f8cba8eee180
Luisa Polanco <luipolanco1229@gmail.com>
2024-09-08 15:50:57 -0500

    some graphic details modified

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("register/", views.register, name="register"),
     path("login/", views.CustomLoginView.as_view(), name="login"),
-    path("logout/", LogoutView.as_view(template_name="logout.html"), name="logout"),
+    path("logout/",views.custom_logout, name="logout"),
 ]
