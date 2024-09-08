b34312fd0e6a5de7b9eee8b84c63e870225784b7
Luisa Polanco <luipolanco1229@gmail.com>
2024-09-08 17:22:38 -0500

    forms language changed and navbar distribution fixed

--- realestatepro/settings.py
+++ realestatepro/settings.py
@@ -108,9 +108,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
-
-TIME_ZONE = "UTC"
+LANGUAGE_CODE = 'es'
+TIME_ZONE = 'America/Lima'
 
 USE_I18N = True
 
